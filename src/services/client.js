1440ab21e792d5cfc9460eac813175fa5a140509
CarolynnFleming <cfcatpink@gmail.com>
2022-05-17 17:10:44 -0700

    parsedata

--- src/services/client.js
+++ src/services/client.js
@@ -4,3 +4,9 @@ export const client = createClient(
     proccess.env.SUPABASE_URL,
     process.env.SUPABASE_KEY
 );
+
+export const parseData = ({ data, error }) => {
+    if (error) throw error;
+
+    return data;
+};
