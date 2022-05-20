2b4f7849601532078abe6d250848d1516f5b63ae
CarolynnFleming <cfcatpink@gmail.com>
2022-05-20 14:56:24 -0700

    set state

--- src/components/Reviews/ReviewDetail.js
+++ src/components/Reviews/ReviewDetail.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 export default function ReviewDetail() {
+cosnt [sG, setSG] = useState({});
+const { id } = useParams();
+
   return (
     <div>ReviewDetail</div>
   )
