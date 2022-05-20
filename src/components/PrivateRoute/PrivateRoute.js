390b829803ed02d2d4a57ad6d8f0493011a4ced0
CarolynnFleming <cfcatpink@gmail.com>
2022-05-20 13:56:13 -0700

    use auth import

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { useAuth } from '../../hooks/user';
+import { Redirect, Route } from 'react-router-dom';
+
 
 export default function PrivateRoute({ children, ...rest }) {
     const { loggedIn } = useAuth();
