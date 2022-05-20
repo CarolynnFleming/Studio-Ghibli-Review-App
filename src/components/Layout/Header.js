77b96911603b107caa8804140ace27607b7662f5
CarolynnFleming <cfcatpink@gmail.com>
2022-05-20 13:44:20 -0700

    links auth button and profile user name in header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useUser, UseAuth } from '../../hooks/user';
 import { useReviewCount } from '../../hooks/reviews';
-import AuthButton from '../../components/Auth/Auth';
+import AuthButton from '../../components/Auth/AuthButton';
 
 export default function Header() {
     const { user, profile, isLoaded } = useUser();
@@ -9,4 +9,23 @@ export default function Header() {
     const count = useReviewCount();
 
     if (loggedIn && !isLoaded) return null;
+
+    return (
+        <>
+        <Header>
+            <Link to='/'>Review the Ghibli Movies</Link>
+            <Link to="/reviews">{count}Review{count !== 1 && 'r'}</Link>
+            <p>{loggedIn ? (
+                <>
+                <span>Signed in as</span>
+                <span>{profile?.username || user.email}</span>
+                </>
+            ) : (
+                <span>Not Signed In</span>
+            )}
+            <AuthButton />
+            </p>
+        </Header>
+        </>
+    );
 }
