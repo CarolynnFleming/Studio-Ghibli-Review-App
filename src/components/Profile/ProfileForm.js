7dc8dcdb266d83297d047027a5701afbdedf5e6b
CarolynnFleming <cfcatpink@gmail.com>
2022-05-20 14:05:19 -0700

    state

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useState } from 'react';
+import { useForm } from '../../hooks/useForm';
 
-export default function ProfileForm() {
+export default function ProfileForm({
+    formLabel = 'Profile Form',
+    username = '',
+    email,
+    onSubmit,
+}) {
+
+    const { formState, handleChange } = useForm({ username, email });
+    const [saving, setSaving] = useState(false);
+
+    
   return (
     <div>ProfileForm</div>
   )
