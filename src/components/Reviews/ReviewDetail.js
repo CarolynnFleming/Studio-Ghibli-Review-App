a12b3d69798caba418d9179db1302be9ab9beff8
CarolynnFleming <cfcatpink@gmail.com>
2022-05-20 15:02:40 -0700

    fetching list of movies

--- src/components/Reviews/ReviewDetail.js
+++ src/components/Reviews/ReviewDetail.js
@@ -5,6 +5,16 @@ export default function ReviewDetail() {
 cosnt [sG, setSG] = useState({});
 const { id } = useParams();
 
+useEffect(() => {
+    const fetchMovie = async() => {
+        const res = await fetch(
+            `https://ghibliapi.herokuapp.com/films/${id}`
+        );
+        const movieData = await res.json();
+        setSG(movieData);
+    }
+    fetchMovie();
+}, []);
   return (
     <div>ReviewDetail</div>
   )
