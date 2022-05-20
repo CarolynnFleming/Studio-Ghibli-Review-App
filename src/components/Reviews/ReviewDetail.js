09395745ea199bf5de3b4b9e45011c318d8b5194
CarolynnFleming <cfcatpink@gmail.com>
2022-05-20 15:23:07 -0700

    returning api details and a spot to make reviews

--- src/components/Reviews/ReviewDetail.js
+++ src/components/Reviews/ReviewDetail.js
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 export default function ReviewDetail({ review, isOwner }) {
-cosnt [sG, setSG] = useState({});
+const [sG, setSG] = useState({});
 const { id } = useParams();
-const { username, movie, review, created } = review;
+const { username, movie, thoughts, created } = review;
 const date = new Date(created).toLocaleDateString();
 
 useEffect(() => {
@@ -13,11 +13,27 @@ useEffect(() => {
             `https://ghibliapi.herokuapp.com/films/${id}`
         );
         const movieData = await res.json();
+        setLoading(false);
         setSG(movieData);
     }
     fetchMovie();
 }, []);
   return (
-    <div>ReviewDetail</div>
+      <>
+    <h1>Movie Review</h1>
+    <Link to="/">Back to the List of Movies</Link>
+    <article>
+        <h2>{sG.title}</h2>
+        <h2>{sG.original_title}</h2>
+        <img src={sG.movie_banner} />
+        <h4>{sG.description}</h4>
+        <h2>Director: {sG.director}</h2>
+        <h2>Producer: {sG.producer}</h2>
+        <h2>{sG.release_date}</h2>
+        <h2>{movie}</h2>
+        <p>Created by {isOwner ? 'you' : username} on {date}</p>
+        <p>{thoughts}</p>
+    </article>
+    </>
   )
 }
