0ee98006acefbee408926fbeaa3fb9d030c59653
CarolynnFleming <cfcatpink@gmail.com>
2022-05-20 17:55:23 -0700

    set state

--- src/components/Reviews/ReviewForm.js
+++ src/components/Reviews/ReviewForm.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useState } from 'react';
+import { useForm } from '../../hooks/useForm';
 
-export default function ReviewForm() {
+
+export default function ReviewForm({
+    review = {},
+    label = 'Review',
+    onSubmit,
+}) {
+
+    const { movie ='', thoughts = '', } = review;
+    const { formState, handleChange } = useForm({ movie, thoughts });
+    const [saving, setSaving] = useState(false);
+
+    
   return (
     <div>ReviewForm</div>
   )
