d1b512e713fe410921b5a3aefd045a799aca4295
CarolynnFleming <cfcatpink@gmail.com>
2022-05-23 09:29:48 -0700

    handle change producer

--- src/components/Reviews/ReviewList.js
+++ src/components/Reviews/ReviewList.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import ReviewItem from "./ReviewItem";
+import { Link, useHistory, useLoocation } from 'react-router-dom'
+import { useState } from "react";
 
-export default function ReviewList() {
+export default function ReviewList({ reviews }) {
+    const location = useHistory();
+    const producer= new URLSeearchPrograms(location.search).get('producer') ?? 'all';
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const handleProduceChange = (e) => {
+        history.pushState(`/?producer=${e.target.value}`);
+    };
+
+    if(!reviews) return null;
   return (
     <div>ReviewList</div>
   )
