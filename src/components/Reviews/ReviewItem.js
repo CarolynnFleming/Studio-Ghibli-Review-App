5323b3c86ac0628ac9dfe89309063700a8919c94
CarolynnFleming <cfcatpink@gmail.com>
2022-05-23 08:41:00 -0700

    set state, created date....ect

--- src/components/Reviews/ReviewItem.js
+++ src/components/Reviews/ReviewItem.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/user';
 
-export default function ReviewItem() {
+export default function ReviewItem({ review }) {
+    const { user } = useAuth();
+    const { id, movie, username, userId, created } = review;
+    const isOwner = user.id === userId;
+    const date = new Date(created);
+    const action = isOwner ? 'edit' : 'copy';
   return (
     <div>ReviewItem</div>
   )
