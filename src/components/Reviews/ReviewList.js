01990dfd8dcfe0c83c35dd7fb0f903b7760cf280
CarolynnFleming <cfcatpink@gmail.com>
2022-05-23 09:37:54 -0700

    fetching movie and searching for producer

--- src/components/Reviews/ReviewList.js
+++ src/components/Reviews/ReviewList.js
@@ -1,6 +1,6 @@
 import ReviewItem from "./ReviewItem";
 import { Link, useHistory, useLoocation } from 'react-router-dom'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ReviewList({ reviews }) {
     const location = useHistory();
@@ -11,6 +11,25 @@ export default function ReviewList({ reviews }) {
     const handleProduceChange = (e) => {
         history.pushState(`/?producer=${e.target.value}`);
     };
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            setLoading(true);
+            const producerParams = new URLSearchParams(location.search).get('producer');
+
+            const url =
+            producerParams === 'all' || !producerParams
+            ?'https://ghibliapi.herokuapp.com/films'
+            : `https://ghibliapi.herokuapp.com/films?producer=${producerParams}`;
+
+            const res = await fetch(url);
+            const sGF = await res.json();
+
+            setMovies(sGF)
+            setLoading(false);
+        }
+        fetchMovies();
+    }, [location.search]);
 
     if(!reviews) return null;
   return (
