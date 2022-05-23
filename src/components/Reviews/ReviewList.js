0719a0d908fd76d375a5471d1c774ecd82a49801
CarolynnFleming <cfcatpink@gmail.com>
2022-05-23 10:23:40 -0700

    return a list of movies and a lisrt of reviews

--- src/components/Reviews/ReviewList.js
+++ src/components/Reviews/ReviewList.js
@@ -33,6 +33,52 @@ export default function ReviewList({ reviews }) {
 
     if(!reviews) return null;
   return (
-    <div>ReviewList</div>
-  )
+    <>
+    <h1>Movie List</h1>
+    {loading ? (
+        <p>Loading Movies...</p>
+    ) : (
+        <section>
+            <label htmlFor="producer">Film Producer:</label>
+            <select id="producer" value={producer} onChange={handleProduceChange}>
+            <option value="all">All</option>
+                <option value="Isao Takahata">Isao Takahata</option>
+                <option value="Toru Hara">Toru Hara</option>
+                <option valu="Hayao Miyacaki">Hayao Miyazaki</option>
+                <option value="Toshio Suzuki">Toshio Suzuki</option>
+                <option value="Yoshio Nishimura">Yoshio Nishimura</option>
+                <option value="Isao Takahata">Isao Takahata</option>
+                <option value="Vincent Maraval">Vincent Maraval</option>
+                <option value="Pascal Caucheteux">Pascal Caucheteux</option>
+                <option value="Gregoire Sorlat">Gregoire Sorlat</option>
+            </select>
+            {movies.map((movie) => ( 
+            <article key={movie.id}>
+                <Link to={`/reviews/${movie.id}`}>
+                    <h3>{movie.title}</h3>
+                </Link>
+                <p>{movie.original_title}</p>
+                <img src={movie.image}/>
+                <h2>Director: {movie.director}</h2>
+                <h2>Producer: {movie.producer}</h2>
+            </article>))}
+        </section>
+    )}
+    <ul>
+        <li>
+            <span>Date</span>
+            <span>Movie</span>
+            <span>Owner</span>
+            <span></span>
+        </li>
+        {reviews.map(review => {
+            return(
+                <ReviewItem
+                key={review.id}
+                review={review} />
+            );
+        })}
+    </ul>
+    </>
+  );
 }
