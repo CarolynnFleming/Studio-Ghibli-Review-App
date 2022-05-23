67769666a5121b73c19a0fdaaf652a2ae5b198f7
CarolynnFleming <cfcatpink@gmail.com>
2022-05-23 10:24:11 -0700

    use form hoook

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useState } from 'react';
+import { useForm } from '../../hooks/useForm';
 
-export default function UserForm() {
+
+export default function UserForm({ className = '', label, onSubmit }) {
+    const { formState, handleChange } = useForm({
+        email: '',
+        password: '',
+    })
   return (
     <div>UserForm</div>
   )
