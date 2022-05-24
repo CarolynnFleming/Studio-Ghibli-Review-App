f002887dee0543fa6f30de54b26635caad22a34d
CarolynnFleming <cfcatpink@gmail.com>
2022-05-24 08:25:44 -0700

    profile path

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import { Redirect, useHistory, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import UserForm from '../../components/UserForm/UserForm';
+import { useAuth } from '../../hooks/user';
+
+const PROFILE_PATH = '/profile';
 
 export default function Auth() {
   return (
