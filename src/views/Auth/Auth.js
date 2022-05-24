57bdff4d0edae2c9c27bf0d433fe60c47b384713
CarolynnFleming <cfcatpink@gmail.com>
2022-05-24 08:28:20 -0700

    state

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -5,7 +5,12 @@ import { useAuth } from '../../hooks/user';
 
 const PROFILE_PATH = '/profile';
 
-export default function Auth() {
+export default function Auth({ signingUp = false }) {
+  const history = useHistory();
+  const location = useLocation();
+  const { loggedIn, signUp, signIn } = useAuth();
+
+
   return (
     <div>Auth</div>
   )
