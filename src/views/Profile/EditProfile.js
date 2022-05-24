0d0a79b0571b6a3d0f1b1d071da07da8997095c5
CarolynnFleming <cfcatpink@gmail.com>
2022-05-24 09:00:30 -0700

    handle edit function

--- src/views/Profile/EditProfile.js
+++ src/views/Profile/EditProfile.js
@@ -8,4 +8,17 @@ export default function EditProfile() {
     const history = useHistory();
     const { profile, update, isLoaded } = useUser();
     const [loading, setLoading] = useState(false);
+
+    if(!isLoaded) return null;
+
+    const handleEdit = async (profile) => {
+        try{
+            setLoading(true);
+            await update(profile);
+            history.push('/profile');
+        } catch (error) {
+            setLoading(false);
+        }
+    };
+    
 }
