fb35194620fabf72c81a250a48b52ebf9efa99c7
CarolynnFleming <cfcatpink@gmail.com>
2022-05-24 07:50:09 -0700

    return previous state and handle change

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -6,6 +6,12 @@ export function useForm(inputs = {}) {
     const handleChange = (e) => {
         const { username, value, type, checked } = e.target;
 
-
-    }
+    setFormState((prevState) => {
+        return {
+            ...prevState,
+            [username]: type === 'checkbox' ? checked : value
+        };
+    });
+    };
+    return { formState, handleChange}
 }
