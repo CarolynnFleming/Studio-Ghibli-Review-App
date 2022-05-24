6b2de41405967a13c87ca13120d76ed87c2bdb02
CarolynnFleming <cfcatpink@gmail.com>
2022-05-24 07:46:41 -0700

    handle change

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -3,5 +3,9 @@ import { useState } from 'react';
 export function useForm(inputs = {}) {
     const [formState, setFormState] = useState({ ...inputs });
 
-    
+    const handleChange = (e) => {
+        const { username, value, type, checked } = e.target;
+
+
+    }
 }
